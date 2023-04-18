266541f375a5935f8c50ff48940f04dc13033093
BeginArjun <jayshrma161@gmail.com>
2023-04-18 20:05:30 +0530

    Added Updates to UI and Added Backend

--- Main.java
+++ Main.java
@@ -2,18 +2,30 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;  
 import checkin.Booking;
+import checkout.Checkout;
 
 public class Main extends JFrame {
     public Main(){
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setTitle("Hotel Management System");
+        setLayout(new BorderLayout());
+        JLabel background=new JLabel(new ImageIcon("home.jpg"));
+        background.setLayout(new GridBagLayout());
         JPanel Menu = new JPanel();
-        Menu.setLayout(new FlowLayout());
+        Menu.setLayout(new BoxLayout(Menu, BoxLayout.X_AXIS));
+        Menu.setBackground(Color.pink);
         JButton checkin = new JButton("CHECK IN");
+        checkin.setBackground(Color.white);
+        checkin.setAlignmentX(Component.CENTER_ALIGNMENT);
         Menu.add(checkin);
         JButton checkout = new JButton("CHECK OUT");
+        checkout.setBackground(Color.white);
+        checkout.setAlignmentX(Component.CENTER_ALIGNMENT);
         Menu.add(checkout);
-        setContentPane(Menu);
+        background.add(Menu);
+        add(background);
+        setBackground(Color.pink);
+        
         checkin.addActionListener(new ActionListener(){  
             public void actionPerformed(ActionEvent e){  
                 Booking book = new Booking();
@@ -25,7 +37,18 @@ public class Main extends JFrame {
             }
         });
 
-        setSize(400, 100);
+        checkout.addActionListener(new ActionListener(){  
+            public void actionPerformed(ActionEvent e){  
+                Checkout cout = new Checkout();
+                setContentPane(cout.getPanel());
+                setSize(400,300);
+                setTitle("Check Out");
+                revalidate();
+                repaint();
+            }
+        });
+
+        setSize(279, 402);
         setLocationRelativeTo(null); 
     }
     public static void main(String[] args) {
--- checkin/Booking.java
+++ checkin/Booking.java
@@ -1,26 +1,81 @@
 package checkin;
 import javax.swing.*;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.awt.*;
-
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import fileHandle.*;
+import Bill.Bill;
 
 public class Booking extends JFrame{
     JPanel panel = new JPanel();
     public Booking() {
         // Create a panel to hold the components
         panel.setLayout(new GridLayout(0, 2));
+        panel.setBackground(Color.decode("#ffd8ec"));
 
         // Add labels and text fields for the reservation details
-        panel.add(new JLabel("Name:"));
-        panel.add(new JTextField());
-        panel.add(new JLabel("Date:"));
-        panel.add(new JTextField());
-        panel.add(new JLabel("Time:"));
-        panel.add(new JTextField());
-        panel.add(new JLabel("Number of Guests:"));
-        panel.add(new JTextField());
+        JLabel nameLabel = new JLabel("Name:");
+        nameLabel.setForeground(Color.decode("#ffffff"));
+        panel.add(nameLabel);
+        JTextField name=new JTextField();
+        panel.add(name);
+        JLabel dateLabel = new JLabel("Date :");
+        dateLabel.setForeground(Color.decode("#ffffff"));
+        panel.add(dateLabel);
+        JFormattedTextField dateField = new JFormattedTextField(new SimpleDateFormat("dd/MM/yyyy"));
+        LocalDate currentDate = LocalDate.now();
+        Date dateValue = Date.from(currentDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+        dateField.setValue(dateValue);
+        panel.add(dateField);
+        JLabel timeLabel = new JLabel("Time:");
+        timeLabel.setForeground(Color.decode("#ffffff"));
+        panel.add(timeLabel);
+        SpinnerDateModel model = new SpinnerDateModel();
+        JSpinner timeSpinner = new JSpinner(model);
+        JSpinner.DateEditor editor = new JSpinner.DateEditor(timeSpinner, "HH:mm");
+        timeSpinner.setEditor(editor);
+        panel.add(timeSpinner);
+        JLabel guestsLabel = new JLabel("Number of guests: ");
+        guestsLabel.setForeground(Color.decode("#ffffff"));
+        panel.add(guestsLabel);
+        JTextField guests=new JTextField();
+        panel.add(guests);
+        
+        // Add a dropdown list for available rooms
+        JLabel roomLabel = new JLabel("Room: ");
+        roomLabel.setForeground(Color.decode("#ffffff"));
+        panel.add(roomLabel);
+        String[] rooms = {"Room 1", "Room 2", "Room 3"};
+        JComboBox<String> roomList = new JComboBox<>(rooms);
+        panel.add(roomList);
 
         // Add a button to submit the reservation
         JButton submitButton = new JButton("Submit Reservation");
+        
+        submitButton.addActionListener(e -> {
+            String selectedRoom = (String) roomList.getSelectedItem();
+            String selectedDate = dateField.getText();
+            if (fileHandle.isRoomAvailable(selectedRoom, selectedDate)) {
+                String[] data = {name.getText(), selectedDate, editor.getFormat().format(timeSpinner.getValue()), guests.getText(), selectedRoom};
+                fileHandle.writeToCSV(data);
+                JOptionPane.showMessageDialog(null, "Reservation submitted!");
+                Bill totalBill=new Bill();
+            JOptionPane.showMessageDialog(null,"Your total Bill is : $"+totalBill.roomBill(Integer.parseInt(guests.getText())));
+                name.setText("");
+                dateField.setValue(dateValue);
+                guests.setText("");
+                roomList.setSelectedIndex(0);
+            } else {
+                JOptionPane.showMessageDialog(null, "Sorry, the selected room is not available on the selected date.");
+            }
+            
+        });
+        
+        submitButton.setBackground(Color.decode("#ffa8cb"));
+        
         panel.add(submitButton);
     }
 
